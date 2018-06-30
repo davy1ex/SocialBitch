61b98ff6543fdb9b56bdbe53e8b9fda9fb514054
davy1ex <alexdavydov88@gmail.com>
2018-07-01 00:06:03 +0400

    update

--- bot.py
+++ bot.py
@@ -9,19 +9,18 @@ ACTION_LIMIT = 190
 
 
 def menu():
+	""" show text menu """
 	print()
 	for index, paragraph in enumerate(["Exit", "Get subs", "Add all subs", "Be bitch"], 0):
 		print("[{0}] {1}".format(index, paragraph), "\n")
-	#print("\n")
 
 def getMe():
-	# global vk
+	""" receives information about the authorized user """
 	info = vk.users.get(fields="nickname")
 	return info
 
-def auth(token=""):
+def auth(token):
 	""" authentication """
-	token = "670b31862e15c59168dcae17c978ff19f622d3183f1d21b650a9852e95a3c0eaf7007f360a448b210c7f4"
 	try:
 		v = VkApi(token=token)
 		lp = VkLongPoll(v)
@@ -37,24 +36,23 @@ def auth(token=""):
 		auth(token=input("Try again: "))
 
 def get_id_followers(user_id=None):
+	""" receives id of subscribers """
 	subs = vk.users.getFollowers()["items"]
-	# print(subs)
 	if len (subs) > 0:
 		for index, user_id in enumerate(subs, 1):
 			print("{0}. {1}".format(index, user_id))
-			# take_in_frends(user_id=id)
 		return subs
 	else:
 		print("Not found")
 		return None
 
 def take_in_frends(user_id, follow=0):
+	""" adds to friends """
 	user_id = int(user_id)
 	if follow == 0 or follow == 1:
 		vk.friends.add(user_id=user_id, follow=follow)
 		global actions
 		actions += 1
-		# vk.messages.send(user_id=user_id, message="жопа")
 	else:
 		return "ERROR: follow must be 1 or 0"
 	if follow == 0:
@@ -63,12 +61,9 @@ def take_in_frends(user_id, follow=0):
 	elif follow == 1:
 		print("Rejected this user's request: {}".format(user_id), "\n")
 
-# 154597302
 if __name__ == "__main__":
-	print("-"*13, " SocialBlyad_bot by davy1ex ", "-"*13)
+	print("-"*13, " SocialBitch_bot by davy1ex ", "-"*13)
 	auth(token=input("Token: "))
-	# getMe()
-	# get_id_followers()
 	menu()
 	choice = ""
 	while choice != "0":
@@ -83,6 +78,3 @@ if __name__ == "__main__":
 		elif choice == "3":
 			print("Coming soon")
 		print("")
-	# take_in_frends(user_id=input(""))
-	#print(vk.users.getFollowers())
-	#print(get_id_followers())
