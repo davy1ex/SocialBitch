be23d2311259f86abb84446a69426e45ab17cb65
davy1ex <32157455+davy1ex@users.noreply.github.com>
2018-07-01 09:44:18 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,79 +7,79 @@ ACTION_LIMIT = 190
 
 
 def menu():
-	""" show text menu """
-	print()
-	for index, paragraph in enumerate(["Exit", "Add all subs", "Be bitch"], 0):
-		print("[{0}] {1}".format(index, paragraph), "\n")
+    """ show text menu """
+    print()
+    for index, paragraph in enumerate(["Exit", "Add all subs", "Be bitch"], 0):
+        print("[{0}] {1}".format(index, paragraph), "\n")
 
 def getMe():
-	""" receives information about the authorized user """
-	info = vk.users.get(fields="nickname")
-	return info
+    """ receives information about the authorized user """
+    info = vk.users.get(fields="nickname")
+    return info
 
 def auth(token):
-	""" authentication """
-	try:
-		v = VkApi(token=token)
-		lp = VkLongPoll(v)
-		global vk
-		vk = v.get_api()
+    """ authentication """
+    try:
+        v = VkApi(token=token)
+        lp = VkLongPoll(v)
+        global vk
+        vk = v.get_api()
 
-		first_name = getMe()[0]["first_name"]
-		last_name = getMe()[0]["last_name"]
-		name = first_name + " " + last_name
-		print("{0}: Authorization completed successfully".format(name))
-	except ApiError:
-		print("ERROR: Invalid token")
-		auth(token=input("Try again: "))
+        first_name = getMe()[0]["first_name"]
+        last_name = getMe()[0]["last_name"]
+        name = first_name + " " + last_name
+        print("{0}: Authorization completed successfully".format(name))
+    except ApiError:
+        print("ERROR: Invalid token")
+        auth(token=input("Try again: "))
 
 def get_id_followers(user_id=None):
-	""" receives id of subscribers """
-	subs = vk.users.getFollowers()["items"]
-	if len (subs) > 0:
-		for index, user_id in enumerate(subs, 1):
-			print("{0}. {1}".format(index, user_id))
-		return subs
-	else:
-		print("Subscribers not found :(")
-		return None
+    """ receives id of subscribers """
+    subs = vk.users.getFollowers()["items"]
+    if len (subs) > 0:
+        for index, user_id in enumerate(subs, 1):
+            print("{0}. {1}".format(index, user_id))
+        return subs
+    else:
+        print("Subscribers not found :(")
+        return None
 
 def take_in_frends(user_id, follow=0):
-	""" adds to friends """
-	user_id = int(user_id)
-	if follow == 0 or follow == 1:
-		vk.friends.add(user_id=user_id, follow=follow)
-		global actions
-		actions += 1
-	else:
-		return "ERROR: follow must be 1 or 0"
-	if follow == 0:
-		print("Add this user: {}".format(user_id))
-		print("Actions today:", actions, "\n")
-	elif follow == 1:
-		print("Rejected this user's request: {}".format(user_id), "\n")
+    """ adds to friends """
+    user_id = int(user_id)
+    if follow == 0 or follow == 1:
+        vk.friends.add(user_id=user_id, follow=follow)
+        global actions
+        actions += 1
+    else:
+        return "ERROR: follow must be 1 or 0"
+    if follow == 0:
+        print("Add this user: {}".format(user_id))
+        print("Actions today:", actions, "\n")
+    elif follow == 1:
+        print("Rejected this user's request: {}".format(user_id), "\n")
 
 if __name__ == "__main__":
-	print("-"*13, " SocialBitch_bot by davy1ex ", "-"*13, "\n")
-	auth(token=input("Token: "))
-	menu()
-	choice = ""
-	while choice != "0":
-		choice = input()
-		if choice == "1":
-			# if actions < ACTION_LIMIT:
-			ids = get_id_followers()
-			if ids != None and actions < ACTION_LIMIT:
-				for user_id in ids:
-					take_in_frends(user_id=user_id)
-			elif actions >= ACTION_LIMIT:
-				print("Exhausted action limit from this account, come back tomorrow")
-		# elif choice == "2":
-		# 	ids = get_id_followers()
-		# 	if ids != None:
-		# 		for user_id in ids:
-		# 			take_in_frends(user_id=user_id)
-		elif choice == "2":
-			print("Coming soon")
-		print("")
-	print("Good bye.")
+    print("-"*13, " SocialBitch_bot by davy1ex ", "-"*13, "\n")
+    auth(token=input("Token: "))
+    menu()
+    choice = ""
+    while choice != "0":
+        choice = input()
+        if choice == "1":
+            # if actions < ACTION_LIMIT:
+            ids = get_id_followers()
+            if ids != None and actions < ACTION_LIMIT:
+                for user_id in ids:
+                    take_in_frends(user_id=user_id)
+            elif actions >= ACTION_LIMIT:
+                print("Exhausted action limit from this account, come back tomorrow")
+        # elif choice == "2":
+        #   ids = get_id_followers()
+        #   if ids != None:
+        #       for user_id in ids:
+        #           take_in_frends(user_id=user_id)
+        elif choice == "2":
+            print("Coming soon")
+        print("")
+    print("Good bye.")
